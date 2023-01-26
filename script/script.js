d8d6e251192d3597211d469f8ed3e999c8b948f6
Ivan Agaev <ivanagaev044@gmail.com>
2023-01-26 22:51:57 +0200

    tabulation_function

--- script/script.js
+++ script/script.js
@@ -44,6 +44,70 @@ function calculateonconsole(a, operator, b) {
   }
 }
 
-calculateonconsole(9, "√", 0);
-calculateonconsole(9, "square", 0);
-calculateonconsole(9, "%", 0 );
+
+// Табулювання функції
+
+//e^-x^2
+function forloop1(x) {
+  return Math.exp(-(Math.pow(Number(x), 2)));
+}
+
+//1 - x^2
+function forloop2(x) {
+  return 1 - Math.pow(Number(x), 2);
+}
+
+//sin2x+cos(3+x)
+function forloop3(x) {
+return Math.sin(2*x) + Math.cos(3+x);
+}
+
+//|x+4|^5
+function forloop4(x) {
+  return Math.abs(Math.pow(Number(x)+4,5));
+  }
+
+  //1 - e^x
+function forloop5(x) {
+  return (1 - Math.exp(Number(x)));
+  }
+
+function loop(a, b, n) {
+
+  let h = (b - a) / n;
+  let p = 0;
+  console.log("f(x) = e^-x^2");
+
+  for (let i = 0; i < 4; i = i + h) {
+    let xi = Number(a) + Number(p) * Number(h);
+    console.log( "x(i):" + xi  + " " + "a:" + a + " " + "b:" + b + " " + "n:" + p++ + " " + "x:" + i + " " + "y:" + forloop1(i)); 
+
+  }
+  console.log("f(x) = 1 - x^2");
+  let r = 0;
+  for (let i = 0; i < 4; i = i + h) {
+    let xi = Number(a) + Number(r) * Number(h);
+    console.log( "x(i):" + xi  + " " + "a:" + a + " " + "b:" + b + " " + "n:" + r++ + " " + "x:" + i + " " + "y:" + forloop2(i));
+  }
+  console.log("f(x) = sin2x+cos(3+x)");
+  let z = 0;
+  for (let i = 0; i < 4; i = i + h) {
+    let xi = Number(a) + Number(z) * Number(h);
+    console.log( "x(i):" + xi  + " " + "a:" + a + " " + "b:" + b + " " + "n:" + z++ + " " + "x:" + i + " " + "y:" + forloop3(i));
+  }
+  console.log("f(x) = |x+4|^5 ");
+  let l = 0;
+  for (let i = 0; i < 4; i = i + h) {
+    let xi = Number(a) + Number(l) * Number(h);
+    console.log( "x(i):" + xi  + " " + "a:" + a + " " + "b:" + b + " " + "n:" + l++ + " " + "x:" + i + " " + "y:" + forloop4(i));
+  } 
+  console.log("f(x) = 1 - e^x");
+  let m = 0;
+  for (let i = 0; i < 4; i = i + h) {
+    let xi = Number(a) + Number(m) * Number(h);
+    console.log( "x(i):" + xi  + " " + "a:" + a + " " + "b:" + b + " " + "n:" + m++ + " " + "x:" + i + " " + "y:" + forloop5(i));
+  } 
+
+}
+
+loop(0, 4, 14); 
